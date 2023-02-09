e85fe1c588529cdbb0ec5c501fe7494e720fcbf7
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2023-02-09 09:12:10 -0500

    second test

--- test/testApiData.py
+++ test/testApiData.py
@@ -1,4 +1,5 @@
 import main
+import database
 import pytest
 
 
@@ -6,4 +7,15 @@ def test_data_from_web():
     result = main.get_wufoo_data()
     for key, value in result.items():
         assert len(value) == 10
+
+
+def test_new_database():
+    con, cursor = database.open_database("create_new_empty_database")
+    database.create_database_table(cursor)
+    result = database.insert_into_cube_table(cursor)
+    assert result
+
+
+
+
 
